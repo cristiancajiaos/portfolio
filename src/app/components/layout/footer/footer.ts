a86583a7ee46ecb33d7ddde7948104bdf2ec52ec
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-26 10:38:14 -0400

    Agregada fecha de creación para mostrar año actual y rango de fechas si el año actual y el año de creación no coinciden

--- src/app/components/layout/footer/footer.ts
+++ src/app/components/layout/footer/footer.ts
@@ -8,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Footer implements OnInit {
 
+  public creationYear: number = 0;
   public currentYear: number = 0;
 
   ngOnInit(): void {
+    this.setDate();
+  }
+
+  private setDate(): void {
+    const creationDate: Date = new Date(2025, 8, 21);
     const currentDate: Date = new Date();
+
+    this.creationYear = creationDate.getFullYear();
     this.currentYear = currentDate.getFullYear();
   }
 
+  public creationCurrentYearsAreEqual(): boolean {
+    return (this.creationYear === this.currentYear)
+  }
 }
